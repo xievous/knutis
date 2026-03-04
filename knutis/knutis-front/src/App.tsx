38745ef6db6d8644775e30d8264b8f7ab42b7a6e
Oscar <oscar.la02@outlook.com>
2026-03-04 19:54:18 +0100

    feat: added route to potluckDetails
    created potluckDetails

--- knutis/knutis-front/src/App.tsx
+++ knutis/knutis-front/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import CreatePotluck from "./pages/CreatePotluck";
+import { PotluckDeatils } from "./pages/PotluckDeatils";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create" element={<CreatePotluck />} />
+      <Route path="/potluck:id" element={<PotluckDeatils />} />
     </Routes>
   );
 }
