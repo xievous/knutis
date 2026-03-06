0b9fb57dcb802b6c482ed22e90162012716256f1
Oscar <oscar.la02@outlook.com>
2026-03-06 14:38:56 +0100

    style: centered details page

--- knutis/knutis-front/src/pages/PotluckDetails.tsx
+++ knutis/knutis-front/src/pages/PotluckDetails.tsx
@@ -33,6 +33,9 @@ export default function PotluckDetails() {
         display: "flex",
         flexDirection: "column",
         gap: 2,
+        width: "50%",
+        marginLeft: "auto",
+        marginRight: "auto",
       }}
     >
       <Box display="flex" flexDirection="row" justifyContent="space-between">
